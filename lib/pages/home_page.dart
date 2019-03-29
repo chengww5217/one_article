87d401cbce115d6a2757606f0748336384bd0450
chengww <chengwang@yunify.com>
2019-03-29 10:32:09 +0800

    Add font size setting

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:one_article/bean/article_bean.dart';
+import 'package:one_article/db/database.dart';
 
 class HomePage extends StatefulWidget {
   final ArticleBean article;
@@ -15,6 +16,8 @@ class _HomePageState extends State<HomePage> {
 
   _HomePageState(this.article);
 
+  double _fontSize = 18;
+
   @override
   Widget build(BuildContext context) {
     if (article == null) {
@@ -22,29 +25,101 @@ class _HomePageState extends State<HomePage> {
       article.data = DataBean();
       article.data.title = "加载中";
       article.data.content = "加载中";
+      article.starred = false;
     }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(article.data.title),
         centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: _bottomMenu),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(article.starred ? Icons.star : Icons.star_border),
+            onPressed: onStarPress,
+          )
+        ],
       ),
       body: ListView(
         children: [
           Text.rich(
               TextSpan(
-                  text: article.data.date == null? "" : "(${article.data.date.curr}，作者：${article.data.author}，字数：${article.data.wc})",
-                  style:
-                      TextStyle(color: Colors.grey, fontSize: 15, height: 1.4)),
+                  text: article.data.date == null
+                      ? ""
+                      : "(${article.data.date.curr}，作者：${article.data.author}，字数：${article.data.wc})",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: _fontSize - 3,
+                      height: 1.4)),
               textAlign: TextAlign.end),
           Text(
             article.data.content,
-            style: TextStyle(fontSize: 18),
+            style: TextStyle(fontSize: _fontSize),
             textAlign: TextAlign.start,
           )
         ],
         padding: const EdgeInsets.all(10),
       ),
     );
+  }
+
+  void onStarPress() {
+    setState(() {
+      article.starred = !article.starred;
+      ArticleProvider().insertOrReplaceToDB(article);
+    });
+  }
+
+  void _bottomMenu() {
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return StatefulBuilder(
+            builder: (context, mSetState) {
+              return GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10))),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "字号",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Expanded(
+                                child: Slider(
+                              onChanged: (double value) {
+                                mSetState(
+                                    () => _fontSize = value.roundToDouble());
+                                setState(() {});
+                              },
+                              divisions: 16,
+                              label: '${_fontSize.round()}',
+                              value: _fontSize,
+                              activeColor: Colors.blue,
+                              min: 10,
+                              max: 26,
+                            )),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  height: 200,
+                  padding: EdgeInsets.all(8),
+                ),
+                onTap: () => false,
+              );
+            },
+          );
+        });
   }
 }
