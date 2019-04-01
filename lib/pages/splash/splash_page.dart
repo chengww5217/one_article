569fcf5c388d0d69887464a436ae54252bcf1997
chengww <chengww5217@163.com>
2019-04-02 02:18:45 +0800

    Update splash

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:one_article/bean/article_bean.dart';
 import 'package:one_article/network/api.dart';
-import 'package:one_article/pages/home_page.dart';
+import 'package:one_article/pages/home_page/home_page.dart';
 
 class SplashPage extends StatefulWidget {
   SplashPage({Key key, this.title}) : super(key: key);
@@ -17,28 +17,26 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
+  void initState() {
     Future.delayed(const Duration(seconds: 3), () => toHome(null));
     Article.today().then((article) {
       toHome(article);
     }).catchError((e) {
       toHome(null);
     });
+    super.initState();
   }
 
   void toHome(ArticleBean article) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage(article)));
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage(article)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
       return Container(
-        color: Colors.blue,
-        child: Center(
-          child: Image(image: AssetImage('assets/images/splash.png')),
-        ),
+        child: Image(image: AssetImage('assets/images/splash.png'), fit: BoxFit.fill,),
       );
     });
   }
